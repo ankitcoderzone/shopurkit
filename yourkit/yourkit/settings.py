3959ee9f06495d36e0280ffff405876c6e2bbd56
Ankit <ankityadav559008@gmail.com>
2023-02-17 01:30:06 +0530

    another commit

--- yourkit/yourkit/settings.py
+++ yourkit/yourkit/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.urkit.in']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
