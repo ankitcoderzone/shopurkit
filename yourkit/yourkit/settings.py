6739f7fdb34e8b1e478d41e790d7d5ebc87bd92b
Ankit yadav <125366378+ankitcoderzone@users.noreply.github.com>
2023-02-17 14:01:21 +0530

    Update settings.py

--- yourkit/yourkit/settings.py
+++ yourkit/yourkit/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 import environ
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.urkit.in']
 
 
 # Application definition
