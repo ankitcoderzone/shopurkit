560d367b2dbd4f07475457951509628d1cb729e9
Ankit <ankityadav559008@gmail.com>
2023-02-18 23:22:35 +0530

    st

--- yourkit/yourkit/settings.py
+++ yourkit/yourkit/settings.py
@@ -146,8 +146,8 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'kit/media')
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'kit/media')
 LOGIN_REDIRECT_URL = '/profile/'
 
 # Default primary key field type
