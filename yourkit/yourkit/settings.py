539ce9504d44261587f0885f7e9bb815a2a494fe
Ankit yadav <125366378+ankitcoderzone@users.noreply.github.com>
2023-02-22 17:08:12 +0530

    Update settings.py

--- yourkit/yourkit/settings.py
+++ yourkit/yourkit/settings.py
@@ -147,8 +147,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'kit/media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/profile/'
 
 # Default primary key field type
